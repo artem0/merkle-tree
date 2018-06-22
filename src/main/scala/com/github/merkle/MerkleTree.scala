1f62836c0646a9ce9c72ed6e206b7d18bb57b6b4
arukavytsia <artem.rukavitsya@gmail.com>
2018-06-22 14:38:47 +0300

    Add inorder tree traversal

--- src/main/scala/com/github/merkle/MerkleTree.scala
+++ src/main/scala/com/github/merkle/MerkleTree.scala
@@ -2,10 +2,30 @@ package com.github.merkle
 
 /**
   * Binary immutable tree of hashes
-  * @param hash root hash value
-  * @param left left subtree
+  *
+  * @param hash  root hash value
+  * @param left  left subtree
   * @param right right subtree
   */
 case class MerkleTree(hash: Block,
                       left: Option[MerkleTree] = Option.empty,
-                      right: Option[MerkleTree] = Option.empty)
+                      right: Option[MerkleTree] = Option.empty) {
+
+
+}
+
+object MerkleTree {
+
+  def inorderRecursive(tree: MerkleTree) {
+    tree match {
+      case MerkleTree(v, None, None) => print(s" Leaf ${blockToHex(v)}")
+      case MerkleTree(v, leftBranch, rightBranch) => {
+        inorderRecursive(leftBranch.head)
+        print(s" Root ${blockToHex(v)}")
+        inorderRecursive(rightBranch.get)
+      }
+    }
+  }
+
+  def blockToHex(hash: Block): String = hash.map("%02x".format(_)).mkString
+}
